84684bb7c4fc8410186a7c744b840c02f983d839
WRHR <wrhreeves@gmail.com>
2021-03-30 21:19:43 -0600

    Add extend class notes

--- basics/classes.py
+++ basics/classes.py
@@ -12,8 +12,24 @@ class User:
   
 # Init user object
 
-dude = User('Some Guy', 25)
-dude.addDecade()
-print(dude.age)
+# dude = User('Some Guy', 25)
+# dude.addDecade()
+# print(dude.age)
 
+# extend class 
 
+class Customer(User):
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+    self.balance = 0
+
+  def set_balance(self, balance):
+    self.balance = balance
+
+sue = Customer('Sue', 29)
+
+sue.set_balance(500)
+sue.addDecade()
+
+print(f'{sue.name} has ${sue.balance} and is now {sue.age}')
